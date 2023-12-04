836902b57d6b7e46808dbfa959f1293f57f23605
Gabe Skorepa <135848915+gskorepa@users.noreply.github.com>
2023-12-03 19:01:52 -0500

    Addition of card animation
    Added a sliding animation to the cards in order to fulfil all the requirements of the project

--- flask-server/.venv/static/script.js
+++ flask-server/.venv/static/script.js
@@ -19,37 +19,36 @@ function newGame() {
 function updateUI(data) {
     console.log('Received data:', data);
 
-    if (data.playerHand && Array.isArray(data.playerHand)) {
-        document.getElementById('player-hand').innerHTML = formatHand(data.playerHand);
-    } else {
-        console.error('Invalid player hand:', data.playerHand);
-        document.getElementById('player-hand').innerHTML = 'Invalid player hand';
-    }
-
-    if (data.dealerHand && Array.isArray(data.dealerHand)) {
-        document.getElementById('dealer-hand').innerHTML = formatHand(data.dealerHand);
-    } else {
-        console.error('Invalid dealer hand:', data.dealerHand);
-        document.getElementById('dealer-hand').innerHTML = 'Invalid dealer hand';
-    }
+    updateHand('player-hand', data.playerHand);
+    updateHand('dealer-hand', data.dealerHand);
 
     document.getElementById('result').innerHTML = data.result || '';
 }
 
+function updateHand(handId, hand) {
+    const handContainer = document.getElementById(handId);
+    const formattedHand = formatHand(hand);
+
+    handContainer.innerHTML = formattedHand;
+
+    // Use setTimeout to trigger the sliding animation
+    setTimeout(() => {
+        const cards = handContainer.querySelectorAll('.card');
+        cards.forEach(card => card.classList.add('slide-in'));
+    }, 10);
+}
+
 function formatHand(hand) {
-    if (Array.isArray(hand)) {
-        return hand.map(card => {
-            const cardImage = getCardImageSrc(card);
-            return `<img class="card" src="${cardImage}" alt="${card.rank} of ${card.suit}" title="${card.rank} of ${card.suit}">`;
-        }).join('');
-    } else {
-        console.error('Invalid hand:', hand);
-        return 'Invalid hand';
-    }
+    return hand.map((card, index) => {
+        const cardImage = getCardImageSrc(card);
+        const isNewCard = card.newlyDrawn ? 'new-card' : '';
+
+        return `<img class="card ${isNewCard}" src="${cardImage}" alt="${card.rank} of ${card.suit}" title="${card.rank} of ${card.suit}">`;
+    }).join('');
 }
 
 function getCardImageSrc(card) {
     const rank = card.rank === '10' ? '10' : card.rank[0]; // Special case for 10
     const suit = card.suit.toLowerCase();
-    return '/images/' + rank + '_of_' + suit + '.png';
+    return `/images/${rank}_of_${suit}.png`;
 }
